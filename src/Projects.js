834b40ede9a6be74ecf686cc1a99e29accf156c0
Nyx Iskandar <xyntechx@gmail.com>
2021-01-10 14:48:50 +0800

    Update Projects.js

--- src/Projects.js
+++ src/Projects.js
@@ -21,22 +21,6 @@ function Projects() {
 
                 <div className="App-link-container">
                     <a
-                        href="https://www.instagram.com/xyntechx/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="App-link">
-                        <div class="module-border-wrap-link">
-                            <div class="module">
-                                <h1 className="App-heading">Learn Python</h1>
-                                <p>Learn Python is a series of posts on my Instagram which aims to teach the basics of Python all the way to its advanced concepts.</p>
-                            </div>
-                        </div>
-                    </a>
-
-                <p></p>
-                <p></p>
-
-                    <a
                         href="https://github.com/xyntechx/Python-Projects"
                         target="_blank"
                         rel="noopener noreferrer"
@@ -81,6 +65,22 @@ function Projects() {
                             <div class="module">
                                 <h1 className="App-heading">My Website</h1>
                                 <p>My website (the one you are at right now) is made using React. I made it because I wanted to experiment with React and frontend development as well as have a site where I can share my journey and projects!</p>
+                            </div>
+                        </div>
+                    </a>
+    
+                    <p></p>
+                    <p></p>
+    
+                    <a
+                        href="https://www.instagram.com/xyntechx/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="App-link">
+                        <div class="module-border-wrap-link">
+                            <div class="module">
+                                <h1 className="App-heading">Learn Python</h1>
+                                <p>Learn Python is a series of posts on my Instagram which aims to teach the basics of Python all the way to its advanced concepts.</p>
                             </div>
                         </div>
                     </a>
