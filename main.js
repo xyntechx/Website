3b22ff997ac6a24f59da9694a2124bde3f099976
xyntechx <xyntechx@gmail.com>
2021-09-11 22:02:47 +0800

    ✨ Beautify Background

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-import './style.css'
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+    canvas: document.querySelector("#bg"),
 });
 
 // Makes 3D background responsive to screen size changes
 // after loading for the first time
-window.addEventListener( 'resize', function() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-})
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -26,43 +31,46 @@ renderer.render(scene, camera);
 
 // Torus #1
 const geometry = new THREE.TorusGeometry(10, 3, 15, 150);
-const material = new THREE.MeshStandardMaterial({ color: 0xff7c1f, wireframe: true });
-const torus = new THREE.Mesh(geometry, material);
+const material = new THREE.MeshStandardMaterial({
+    color: 0xff7c1f,
+    wireframe: true,
+});
 
-scene.add(torus);
+const torus = new THREE.Mesh(geometry, material);
+torus.position.z = -20;
 
 // Torus Knot #1
 const geometry2 = new THREE.TorusKnotGeometry(5, 1, 100, 16);
-const material2 = new THREE.MeshStandardMaterial({ color: 0x067eed, wireframe: true });
+const material2 = new THREE.MeshStandardMaterial({
+    color: 0x067eed,
+    wireframe: true,
+});
+
 const torusKnot = new THREE.Mesh(geometry2, material2);
-
-scene.add(torusKnot);
-
-torusKnot.position.x = -10;
-torusKnot.position.y = 0;
 torusKnot.position.z = 15;
 
 // Torus #2
 const geometry3 = new THREE.TorusGeometry(5, 1, 15, 100);
-const material3 = new THREE.MeshStandardMaterial({ color: 0x3ded87, wireframe: true });
+const material3 = new THREE.MeshStandardMaterial({
+    color: 0x3ded87,
+    wireframe: true,
+});
+
 const torus2 = new THREE.Mesh(geometry3, material3);
-
-scene.add(torus2);
-
-torus2.position.x = 0;
-torus2.position.y = -5;
-torus2.position.z = 25;
+torus2.position.x = 10;
+torus2.position.y = -5
+torus2.position.z = 30;
 
 // Torus Knot #2
 const geometry4 = new THREE.TorusKnotGeometry(10, 3, 100, 16);
-const material4 = new THREE.MeshStandardMaterial({ color: 0xffff00, wireframe: true });
+const material4 = new THREE.MeshStandardMaterial({
+    color: 0xffff00,
+    wireframe: true,
+});
+
 const torusKnot2 = new THREE.Mesh(geometry4, material4);
-
-scene.add(torusKnot2);
-
-torusKnot2.position.x = -12;
-torusKnot2.position.y = 0;
-torusKnot2.position.z = 50;
+torusKnot2.position.x = 20;
+torusKnot2.position.z = 70;
 
 // Point Light
 const pointLight = new THREE.PointLight(0xffffff);
@@ -70,62 +78,61 @@ pointLight.position.set(5, 5, 5);
 
 // Ambient Light
 const ambientLight = new THREE.AmbientLight(0xffffff);
-scene.add(pointLight, ambientLight);
 
 // Generates white spheres of random sizes and positions
-function addStar() {
-  const geometry = new THREE.SphereGeometry(Math.random()*0.5, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-  const star = new THREE.Mesh(geometry, material);
+const addStar = () => {
+    const geometry = new THREE.SphereGeometry(Math.random() * 0.5, 30, 30);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3)
-    .fill()
-    .map(() => THREE.MathUtils.randFloatSpread(100));
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(500));
 
-  star.position.set(x, y, z);
-  scene.add(star);
-}
-
-// Generates 200 stars
-Array(200).fill().forEach(addStar);
+    star.position.set(x, y, z);
+    scene.add(star);
+};
 
 // Creates animation when camera is moved (i.e. during scrolling)
-function animateScrolling() {
-  const t = document.body.getBoundingClientRect().top;
+const animateScrolling = () => {
+    const t = document.body.getBoundingClientRect().top;
 
-  torus.rotation.x += 0.05;
+    torus.rotation.x += 0.05;
+    torusKnot.rotation.z += 0.05;
+    torusKnot.rotation.x += 0.05;
+    torus2.rotation.y += 0.05;
+    torusKnot2.rotation.z += 0.05;
 
-  torusKnot.rotation.z += 0.05;
+    camera.position.z = t * -0.02;
+    camera.position.x = t * -0.01;
+    camera.rotation.y = t * -0.001;
+};
 
-  torus2.rotation.y += 0.05;
+// Creates animation continuously
+const animate = () => {
+    requestAnimationFrame(animate);
 
-  torusKnot.rotation.x += 0.05;
+    torus.rotation.y += 0.01;
+    torus.rotation.z += 0.01;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0003;
-  camera.rotation.y = t * -0.0003;
-}
+    torusKnot.rotation.x += 0.02;
+    torusKnot.rotation.y += 0.02;
+
+    torus2.rotation.x += 0.03;
+    torus2.rotation.z += 0.02;
+
+    torusKnot2.rotation.y += 0.01;
+    torusKnot2.rotation.z += 0.007;
+
+    renderer.render(scene, camera);
+};
+
+// Generates 300 stars
+Array(300).fill().forEach(addStar);
+
+scene.add(torus, torusKnot, torus2, torusKnot2, pointLight, ambientLight);
 
 document.body.onscroll = animateScrolling;
 animateScrolling();
-
-// Creates animation continuously
-function animate() {
-  requestAnimationFrame(animate);
-
-  torus.rotation.y += 0.01;
-  torus.rotation.z += 0.01;
-
-  torusKnot.rotation.x += 0.02;
-  torusKnot.rotation.y += 0.02;
-
-  torus2.rotation.x += 0.03;
-  torus2.rotation.z += 0.02;
-
-  torusKnot2.rotation.y += 0.01;
-  torusKnot2.rotation.z += 0.007;
-
-  renderer.render(scene, camera);
-}
 
 animate();
