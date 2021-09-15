226a4614ea6e0482b50c08f818b8fc7f984f0374
xyntechx <xyntechx@gmail.com>
2021-09-15 20:54:38 +0800

    🔖 15/09/2021

--- main.js
+++ main.js
@@ -136,3 +136,26 @@ document.body.onscroll = animateScrolling;
 animateScrolling();
 
 animate();
+
+// Image slideshow
+const slideshow = () => {
+    var slides = document.getElementsByClassName("slides");
+
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+
+    slides[slideIndex - 1].style.display = "inline";
+
+    // Swaps image every 2 seconds
+    setTimeout(slideshow, 2000);
+};
+
+var slideIndex = 0;
+slideshow();
