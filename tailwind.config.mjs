0cdb3504c68cf175c88073c81ff1ba43c206c4b5
xyntechx <xyntechx@gmail.com>
2024-06-20 06:09:16 -0700

    🔨 Refactor tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,7 @@
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {
-            backgroundImage: {
-                triangle: "url('/triangle.svg')",
-            },
-        },
+        extend: {},
     },
     plugins: [],
 };
