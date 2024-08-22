48f028d02f6d47340d337bbcf370f5beb85f4a75
xyntechx <xyntechx@gmail.com>
2024-08-22 07:12:49 -0700

    📇 Update deps + enable analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercelStatic from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
 });
