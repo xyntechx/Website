3f2fefb1dd699f2354b52d09eb243fc71471cd05
xyntechx <xyntechx@gmail.com>
2025-09-22 17:15:13 -0700

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  adapter: vercel(),
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
 });
