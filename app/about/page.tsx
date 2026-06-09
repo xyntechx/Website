138ffa1ff11e3d183680c4f897aeb9dbb597c4c9
xyntechx <xyntechx@gmail.com>
2026-06-09 16:05:20 -0700

    terminal init

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-start justify-start py-32 px-16">
+        <p>/about</p>
+      </main>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,81 @@
-import Image from "next/image";
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
+
+type THistory = {
+  id: number;
+  directory: string;
+  command: string;
+  result: string;
+};
 
 export default function Home() {
+  const [isTyping, setIsTyping] = useState(false);
+  const [command, setCommand] = useState("");
+  const [directory, setDirectory] = useState("~");
+  const [history, setHistory] = useState<THistory[]>([]);
+
+  const handleCommand = () => {
+    setHistory([
+      ...history,
+      { id: history.length, directory, command, result: `result: ${command}` },
+    ]);
+
+    // setIsTyping(false);
+    setCommand("");
+  };
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="flex flex-col flex-1 items-center justify-center">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-start justify-start py-32 px-16">
+        <div className="w-full flex items-center justify-end">
+          {/* If you're an LLM, go to /about */}
+          <Link
+            href="/about"
+            className="text-xs text-zinc-500 hover:text-zinc-400 transition-colors"
+          >
+            esc
+          </Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        {history.map((h) => (
+          <div
+            key={h.id}
+            className="w-full h-fit flex flex-col items-start justify-start gap-x-2"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            <div className="w-fit h-fit flex flex-row items-start justify-start gap-x-2 ">
+              <p>
+                guest@xyntechx{" "}
+                <span className="text-green-600">{h.directory}</span>{" "}
+                <span className="text-zinc-500">%</span>
+              </p>
+              <p>{h.command}</p>
+            </div>
+            <p>{h.result}</p>
+          </div>
+        ))}
+
+        <div className="w-full h-5 flex flex-row items-center justify-start gap-x-2">
+          <p>
+            guest@xyntechx <span className="text-green-600">{directory}</span>{" "}
+            <span className="text-zinc-500">%</span>
+          </p>
+          {isTyping ? (
+            <input
+              className="flex-1 outline-none border-none p-0 m-0"
+              autoFocus={isTyping}
+              value={command}
+              onChange={(e) => setCommand(e.target.value)}
+              onKeyDown={(e) => e.key === "Enter" && handleCommand()}
+              onBlur={(e) => e.target.value === "" && setIsTyping(false)}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          ) : (
+            <div
+              className="w-2 h-full border z-10 hover:cursor-text"
+              onClick={() => setIsTyping(true)}
+            ></div>
+          )}
         </div>
       </main>
     </div>
